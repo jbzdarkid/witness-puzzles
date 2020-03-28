d2b8cde6f169d773dc229cbd70d6c9e5bc12793e
jbzdarkid <jbzdarkid@gmail.com>
2020-03-28 13:14:58 -0700

    WIP on optimizer

--- pages/_UTM.js
+++ pages/_UTM.js
@@ -21,6 +21,7 @@ function loadPuzzleRegion(region) {
 }
 
 function loadSinglePuzzle(puzzleData) {
+  document.getElementById('solutionViewer').style.display = 'none'
   puzzle = Puzzle.deserialize(puzzleData)
   window.draw(puzzle)
 }
@@ -31,20 +32,74 @@ function _showSolution(solutions, num) {
 
   var previousSolution = document.getElementById('previousSolution')
   var solutionCount = document.getElementById('solutionCount')
+  var solutionInfo = document.getElementById('solutionInfo')
   var nextSolution = document.getElementById('nextSolution')
 
-  solutionCount.innerText = (num + 1) + ' of ' + solutions.length
-  previousSolution.onclick = function() {_showSolution(solutions, num - 1)}
-  nextSolution.onclick = function() {_showSolution(solutions, num + 1)}
+  // Buttons & text
+  if (solutions.length < 2) { // 0 or 1 solution(s), arrows are useless
+    solutionCount.innerText = solutions.length + ' of ' + solutions.length
+    previousSolution.disabled = true
+    nextSolution.disabled = true
+  } else {
+    solutionCount.innerText = (num + 1) + ' of ' + solutions.length
+    solutionInfo.innerText = solutions[num]['edges'] + ' edges, ' + solutions[num]['corners'] + ' corners'
+    previousSolution.onclick = function() {_showSolution(solutions, num - 1)}
+    nextSolution.onclick = function() {_showSolution(solutions, num + 1)}
+  }
 
-  window.draw(solutions[num])
+  window.draw(solutions[num]['solution'])
   document.getElementById('solutionViewer').style.display = null
+}
+
+function isColored(grid, x, y) {
+  var cell = grid[x][y]
+  return (cell != null && cell.color == 1)
 }
 
 function solvePuzzle() {
   var solutions = window.solve(puzzle)
-  // TODO: Some way of sorting solutions
-  // https://github.com/jbzdarkid/witness-puzzles/blob/master/legacy/_UTM.js#L425
+  for (var i=0; i<solutions.length; i++) {
+    var solution = solutions[i]
+    var edges = 0
+    var corners = 0
+    for (var x=0; x<solution.grid.length; x++) {
+      for (var y=0; y<solution.grid[x].length; y++) {
+        if (x%2 == 1 && y%2 == 1) continue
+        var cell = solution.grid[x][y]
+        if (cell == null || cell.color == 0) continue
+
+        var topCell = solution.grid[x][y-1]
+        var bottomCell = solution.grid[x][y+1]
+        var leftCell = solution.grid[x-1] && solution.grid[x-1][y]
+        var rightCell = solution.grid[x+1] && solution.grid[x+1][y]
+
+        if (x%2 != y%2) edges++
+
+        if (leftCell == null) {
+          if (cell.dir == 'right') corners++
+        } else if (rightCell == null) {
+          if (cell.dir == 'left') corners++
+        } else if (topCell == null) {
+          if (cell.dir == 'bottom') corners++
+        } else if (bottomCell == null) {
+          if (cell.dir == 'top') corners++
+        } else { // Not touching any walls
+          if (cell.dir == 'left' && leftCell.dir == 'left') {}
+          else if (cell.dir == 'right' && rightCell.dir == 'right') {}
+          else if (cell.dir == 'top' && topCell.dir == 'top') {}
+          else if (cell.dir == 'bottom' && bottomCell.dir == 'bottom') {}
+          else corners++ // Direction change
+        }
+      }
+    }
+    solutions[i] = {
+      'sort': edges*100 + corners,
+      'edges': edges,
+      'corners': corners,
+      'solution': solution
+    }
+  }
+
+  solutions.sort(function(a, b){return a['sort']- b['sort']})
   _showSolution(solutions, 0)
 }
-
