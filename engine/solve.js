99bdf416172d4b46852a0e50efe0f3e378c06d13
jbzdarkid <jbzdarkid@gmail.com>
2020-06-29 14:19:57 -0700

    Very long comment. And better variable names.

--- engine/solve.js
+++ engine/solve.js
@@ -17,8 +17,10 @@ function solve(puzzle) {
   }
 
   var solutions = []
+  // Some reasonable default data, which will avoid crashes during the solveLoop.
+  var earlyExitData = [false, {'isEdge': false}, {'isEdge': false}]
   for (var pos of startPoints) {
-    _solveLoop(puzzle, pos.x, pos.y, solutions, numEndpoints, [false, false, null, null])
+    _solveLoop(puzzle, pos.x, pos.y, solutions, numEndpoints, earlyExitData)
   }
 
   var end = (new Date()).getTime()
@@ -28,7 +30,7 @@ function solve(puzzle) {
 
 // @Performance: This is the most central loop in this code.
 // Any performance efforts should be focused here.
-function _solveLoop(puzzle, x, y, solutions, numEndpoints, magic) {
+function _solveLoop(puzzle, x, y, solutions, numEndpoints, earlyExitData) {
   // Stop trying to solve once we reach our goal
   if (solutions.length >= window.MAX_SOLUTIONS) return
 
@@ -49,15 +51,41 @@ function _solveLoop(puzzle, x, y, solutions, numEndpoints, magic) {
     puzzle.updateCell(sym.x, sym.y, {'color':3})
   }
 
+  // Large optimization -- Attempt to early exit once we cut out a region.
+  // For non-pillar puzzles, every time we draw a line from one edge to another, we cut out two regions.
+  // We can detect this by asking if we've ever left an edge, and determining if we've just touched an edge.
+  // However, just touching the edge isn't sufficient, since we could still enter either region.
+  // As such, we wait one additional step, to see which half we have moved in to, then we evaluate
+  // whichever half you moved away from (since you can no longer re-enter it).
+  //
+  // Consider this pathway (tracing X-X-X-A-B-C).
+  // ....X....
+  // . . X . .
+  // ....X....
+  // . . A . .
+  // ...CB....
+  //
+  // Note that, once we have reached B, the puzzle is divided in half. However, we could go either
+  // left or right -- so we don't know which region is safe to validate.
+  // Once we reach C, however, the region to the right is guaranteed to be un-enterable.
+  // As such, we can start a flood fill from the cell to the right of A, computed by A+(C-B).
+  //
+  // Unfortunately, this optimization doesn't work for pillars, since the two regions are the same.
   if (puzzle.pillar === false) {
     var isEdge = x <= 0 || y <= 0 || x >= puzzle.grid.length - 1 || y >= puzzle.grid[0].length - 1
-    // [hasEverTouchedEdge^, hasEverTouchedEdge, lastPos, currentPos]
-    var newMagic = [magic[1], isEdge || magic[1], magic[3], {'x':x, 'y':y, 'isEdge':isEdge}]
-    if (magic[0] && magic[2] && !magic[2].isEdge && magic[3].isEdge && isEdge) {
-      var floodX = magic[3].x - x + magic[2].x
-      var floodY = magic[3].y - y + magic[2].y
+    var newEarlyExitData = [
+      earlyExitData[0] || (!isEdge && earlyExitData[2].isEdge), // Have we ever left an edge?
+      earlyExitData[2],                                         // The position before our current one
+      {'x':x, 'y':y, 'isEdge':isEdge}                           // Our current position.
+    ]
+    if (earlyExitData[0] && !earlyExitData[1].isEdge && earlyExitData[2].isEdge && isEdge) {
+      // Compute the X and Y of the region we just cut out.
+      // This is determined by looking at the delta between the current and last points,
+      // then replaying the *inverse* of that delta against the second-to-last point.
+      var regionX = earlyExitData[2].x + (earlyExitData[1].x - x)
+      var regionY = earlyExitData[2].y + (earlyExitData[1].y - y)
 
-      var region = puzzle.getRegion(floodX, floodY)
+      var region = puzzle.getRegion(regionX, regionY)
       if (!window._regionCheckNegations(puzzle, region).valid) {
         // @Cutnpaste
         // Tail recursion: Back out of this cell
@@ -70,7 +98,7 @@ function _solveLoop(puzzle, x, y, solutions, numEndpoints, magic) {
       }
     }
   } else {
-    var newMagic = magic // Unused, just make a cheap copy.
+    var newEarlyExitData = earlyExitData // Unused, just make a cheap copy.
   }
 
   if (cell.end != undefined) {
@@ -98,16 +126,16 @@ function _solveLoop(puzzle, x, y, solutions, numEndpoints, magic) {
   // Extend path left and right
   if (y%2 === 0) {
     puzzle.updateCell(x, y, {'dir':'left'})
-    _solveLoop(puzzle, x - 1, y, solutions, numEndpoints, newMagic)
+    _solveLoop(puzzle, x - 1, y, solutions, numEndpoints, newEarlyExitData)
     puzzle.updateCell(x, y, {'dir':'right'})
-    _solveLoop(puzzle, x + 1, y, solutions, numEndpoints, newMagic)
+    _solveLoop(puzzle, x + 1, y, solutions, numEndpoints, newEarlyExitData)
   }
   // Extend path up and down
   if (x%2 === 0) {
     puzzle.updateCell(x, y, {'dir':'top'})
-    _solveLoop(puzzle, x, y - 1, solutions, numEndpoints, newMagic)
+    _solveLoop(puzzle, x, y - 1, solutions, numEndpoints, newEarlyExitData)
     puzzle.updateCell(x, y, {'dir':'bottom'})
-    _solveLoop(puzzle, x, y + 1, solutions, numEndpoints, newMagic)
+    _solveLoop(puzzle, x, y + 1, solutions, numEndpoints, newEarlyExitData)
   }
 
   // Tail recursion: Back out of this cell
