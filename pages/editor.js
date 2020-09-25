f679675a454d8c8d27afbd9e886b383fb8ce00fe
jbzdarkid <jbzdarkid@gmail.com>
2020-09-24 22:36:54 -0700

    Fix double tab on ios

--- pages/editor.js
+++ pages/editor.js
@@ -121,8 +121,8 @@ function drawPuzzle() {
       rect.setAttribute('opacity', 0)
       yPos += height
       addOnClick(rect, x, y)
-      rect.onmouseover = function() {this.setAttribute('opacity', 0.25)}
-      rect.onmouseout = function() {this.setAttribute('opacity', 0)}
+      rect.onpointerenter = function() {this.setAttribute('opacity', 0.25)}
+      rect.onpointerleave = function() {this.setAttribute('opacity', 0)}
     }
     xPos += width
   }
