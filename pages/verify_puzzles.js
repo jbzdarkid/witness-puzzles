17384b80b3e64472ce4229b132d2c46cfa3d2015
jbzdarkid <jbzdarkid@gmail.com>
2020-10-19 14:40:10 -0700

    More stuff.

--- pages/verify_puzzles.js
+++ pages/verify_puzzles.js
@@ -7,42 +7,50 @@ namespace(function() {
 // 5. Open the debugger, input the solution (or fix the solution) based on the log output
 // 6. Copy the logged statement into MySQL Workbench and run it
 
-window.onload = function() {
-  setTimeout(function() {
-    for (row of window.puzzles) {
-      var hash = row[0]
-      var puzzle_json = row[2]
-      var solution_json = row[3]
-      var title = row[5]
+function verifyPuzzle(hash, date, puzzle_json, solution_json, url, title) {
+  var solution = Puzzle.deserialize(solution_json)
+  window.validate(solution)
+  if (!solution.valid) {
+    // Unescape HTML (https://paulschreiber.com/blog/2008/09/20)
+    var temp = document.createElement('div')
+    temp.innerHTML = title
+    document.getElementById('title').innerText = temp.childNodes[0].nodeValue + ' (hash: ' + hash + ')'
 
-      var solution = Puzzle.deserialize(solution_json)
-      window.validate(solution)
-      if (!solution.valid) {
-        // Unescape HTML (https://paulschreiber.com/blog/2008/09/20)
-        var temp = document.createElement('div')
-        temp.innerHTML = title
-        document.getElementById('title').innerText = temp.childNodes[0].nodeValue + ' (hash: ' + hash + ')'
-
-        solution.logGrid()
-        var puzzle = Puzzle.deserialize(puzzle_json)
-        window.draw(puzzle)
-        window.TRACE_COMPLETION_FUNC = function(solution) {
-          var sql_statement = 'update puzzle\n' +
-          'set solution_json = \'' + solution.serialize() + '\'\n' +
-          'where display_hash = \'' + hash + '\'\n'
-          console.info(sql_statement)
-        }
-
-        window.deletePuzzle = function() {
-          var sql_statement = 'delete from puzzle\n' +
-          'where display_hash = \'' + hash + '\'\n'
-          console.info(sql_statement)
-        }
-
-        break
-      }
+    solution.logGrid()
+    var puzzle = Puzzle.deserialize(puzzle_json)
+    window.draw(puzzle)
+    window.TRACE_COMPLETION_FUNC = function(solution) {
+      var sql_statement = 'update puzzle\n' +
+      'set solution_json = \'' + solution.serialize() + '\'\n' +
+      'where display_hash = \'' + hash + '\'\n'
+      console.info(sql_statement)
     }
-  }, 100)
+
+    window.deletePuzzle = function() {
+      var sql_statement = 'delete from puzzle\n' +
+      'where display_hash = \'' + hash + '\'\n'
+      console.info(sql_statement)
+    }
+  }
+  return solution.valid
+}
+
+var i = 0
+window.onload = function() {
+  function validateOnePuzzle() {
+    if (i >= window.puzzles.length) {
+      document.getElementById('title').innerText = 'All puzzles valid!'
+      return
+    }
+
+    var row = window.puzzles[i]
+    i++
+    document.getElementById('title').innerText = 'Validating ' + row[0] + '...'
+    var valid = verifyPuzzle(row[0], row[1], row[2], row[3], row[4], row[5])
+    if (!valid) return
+    setTimeout(validateOnePuzzle, 10)
+  }
+  validateOnePuzzle()
 }
 
 window.findPuzzle = function() {
