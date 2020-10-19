76fd3f572f7387c23c8b1438aa432e20520c8803
jbzdarkid <jbzdarkid@gmail.com>
2020-10-19 15:24:34 -0700

    Note to past self.

--- pages/verify_puzzles.js
+++ pages/verify_puzzles.js
@@ -8,16 +8,18 @@ namespace(function() {
 // 6. Copy the logged statement into MySQL Workbench and run it
 
 function verifyPuzzle(hash, date, puzzle_json, solution_json, url, title) {
+  var puzzle = Puzzle.deserialize(puzzle_json)
   var solution = Puzzle.deserialize(solution_json)
-  window.validate(solution)
+
+  // Note: Do not fix up puzzles here! If there is a bug with bad/malformed puzzle data, fix the deserializer.
+
+  window.validate(solution, true)
   if (!solution.valid) {
     // Unescape HTML (https://paulschreiber.com/blog/2008/09/20)
     var temp = document.createElement('div')
     temp.innerHTML = title
     document.getElementById('title').innerText = temp.childNodes[0].nodeValue + ' (hash: ' + hash + ')'
 
-    solution.logGrid()
-    var puzzle = Puzzle.deserialize(puzzle_json)
     window.draw(puzzle)
     window.TRACE_COMPLETION_FUNC = function(solution) {
       var sql_statement = 'update puzzle\n' +
@@ -31,6 +33,10 @@ function verifyPuzzle(hash, date, puzzle_json, solution_json, url, title) {
       'where display_hash = \'' + hash + '\'\n'
       console.info(sql_statement)
     }
+
+    setLogLevel('log')
+    window.validate(solution, false)
+    solution.logGrid()
   }
   return solution.valid
 }
