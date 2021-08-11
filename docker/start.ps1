b6664dcf4877625b08387b26779362e9ec533a4a
Joseph Blackman <joblac@microsoft.com>
2021-08-10 23:56:44 -0700

    Getting started with docker

---
+++ docker/start.ps1
@@ -0,0 +1,9 @@
+$repo_root = $(git rev-parse --show-toplevel).Trim()
+pushd $repo_root
+
+$old_id = $(docker ps -qf "name=witness-puzzles")
+if ($old_id -ne $null) {
+  docker rm -f $old_id.Trim()
+}
+docker build -t witness-puzzles -f docker/Dockerfile .
+docker run -dit --name witness-puzzles -p 8080:80 witness-puzzles
---
+++ docker/wp.wsgi
@@ -0,0 +1,1 @@
+from witness-puzzles import application
