dc424b6941d64cb3eaa2652eedb730c446bafa8b
Joseph Blackman <joblac@microsoft.com>
2021-08-13 02:50:34 -0700

    Apache works! I wish wp worked though.

--- docker/start.ps1
+++ docker/start.ps1
@@ -1,15 +1,13 @@
+$ErrorActionPreference="Stop" # set -e
+
 $repo_root = $(git rev-parse --show-toplevel).Trim()
 pushd $repo_root
 
 docker build -t witness-puzzles-img -f docker/Dockerfile .
-docker run -dit --name tmp -p 8080:80 witness-puzzles-img
 
-$new_id = $(docker ps -aqf "name=tmp")
-if ($new_id -ne $null) {
-  $old_id = $(docker ps -aqf "name=witness-puzzles")
-  if ($old_id -ne $null) {
-    docker rm -f $old_id
-  }
-  docker rename tmp witness-puzzles
-  docker exec -it $new_id /bin/bash
-}
+Write-Host "Build succeeded, replacing old server and connecting to docker container"
+docker stop witness-puzzles | Out-Null
+docker run -dit --name tmp -p 8080:80 witness-puzzles-img | Out-Null
+docker rm witness-puzzles | Out-Null
+docker rename tmp witness-puzzles | Out-Null
+docker exec -it witness-puzzles /bin/bash
