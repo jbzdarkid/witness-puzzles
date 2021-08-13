5c276cf94caf986f2eb3fbc8c79770670f502465
Joseph Blackman <joblac@microsoft.com>
2021-08-13 02:30:34 -0700

    eh

--- docker/start.ps1
+++ docker/start.ps1
@@ -4,10 +4,12 @@ pushd $repo_root
 docker build -t witness-puzzles-img -f docker/Dockerfile .
 docker run -dit --name tmp -p 8080:80 witness-puzzles-img
 
-$old_id = $(docker ps -aqf "name=witness-puzzles")
 $new_id = $(docker ps -aqf "name=tmp")
-if ($old_id -ne $null) {
-  docker rm -f $old_id.Trim()
+if ($new_id -ne $null) {
+  $old_id = $(docker ps -aqf "name=witness-puzzles")
+  if ($old_id -ne $null) {
+    docker rm -f $old_id
+  }
+  docker rename tmp witness-puzzles
+  docker exec -it $new_id /bin/bash
 }
-docker rename tmp witness-puzzles
-docker exec -it $new_id /bin/bash
