caf65a5c4c5aa6ca906ee6863d6760be840e07cf
Joseph Blackman <joblac@microsoft.com>
2021-09-01 05:23:47 -0700

    Progress, of a sort

--- docker/code-deploy.sh
+++ docker/code-deploy.sh
@@ -1,29 +1,38 @@
+# First, clone & download this script
+# git clone https://github.com/jbzdarkid/witness-puzzles
+# chmod u+x ./witness-puzzles/docker/code-deploy.sh
+# sudo ./witness-puzzles/docker/code-deploy.sh
+
+REPO="/home/ubuntu/witness-puzzles" # ... somehow
+mkdir -p /var/www/apache-flask/app/
 cd /var/www/apache-flask/app/
 
-sudo apt-get update
-sudo apt-get install -y \
+apt-get update
+apt-get install -y \
   apache2 \
   build-essential \
   libapache2-mod-wsgi-py3 \
   python3-pip \
+  python3-venv \
   ruby-full \
   vim \
   wget
-sudo apt-get autoremove
+apt-get autoremove
 
 if [ ! -d "venv" ]; then
   python3 -m venv venv
 fi
 
-source ./venv/scripts/activate
-python3 -m pip install -r requirements.txt
+source ./venv/bin/activate
+python3 -m pip install -r "$REPO/requirements.txt"
 
 # Set up Apache
-cp -f docker/apache-flask.conf /etc/apache2/sites-available/
-cp -f docker/apache-flask.wsgi /var/www/apache-flask/app
+cp -rf "$REPO/." .
+cp -f "$REPO/docker/apache-flask.conf" /etc/apache2/sites-available/
+cp -f "$REPO/docker/apache-flask.wsgi" /var/www/apache-flask/app
 a2ensite apache-flask
 a2enmod headers
 a2dissite 000-default.conf
+systemctl restart apache2
 
-cd /var/www/apache-flask/app
 /usr/sbin/apache2ctl -D FOREGROUND
