17bcaef2f4e2da84d1807c82fe4fa8c9bbdf2190
Joseph Blackman <joblac@microsoft.com>
2021-09-12 16:45:46 -0700

    we're going to do this all again anyways

--- code-deploy.sh
+++ code-deploy.sh
@@ -3,20 +3,29 @@
 # chmod u+x ./witness-puzzles/docker/code-deploy.sh
 # sudo ./witness-puzzles/docker/code-deploy.sh
 
-REPO="/home/ubuntu/witness-puzzles" # Step 1 is actually to *move ourself* into the folder.
-mkdir -p /var/www/apache-flask/app/
-cp -rf "$($pwd)/." /var/www/apache-flask/app/.
+# Step 1 is actually to *move ourself* into the folder.
+# In fact, I think the long term plan is to move ourselves into /var/ww/apache-flask/witness-puzzes
+REPO="/home/ubuntu/witness-puzzles"
+# Move files into place
+mkdir -p /var/www/apache-flask/
+REPO=$(git rev-parse --show-toplevel)
+mv -rf "$REPO" /var/www/apache-flask/
+chown $USER witness-puzzles
 
 apt-get update
 apt-get install -y \
   apache2 \
   build-essential \
   libapache2-mod-wsgi-py3 \
+  msql-client \
   python3-pip \
   python3-venv \
   ruby-full \
   vim \
-  wget
+  wget \
+  xdg-utils
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
 apt-get autoremove
 
 # if [ ! -d "venv" ]; then
@@ -26,8 +35,6 @@ apt-get autoremove
 # source ./venv/bin/activate
 python3 -m pip install -r "$REPO/requirements.txt"
 
-# Move files into place
-cp -rf "$REPO/." .
 cp -f "$REPO/docker/apache-flask.conf" /etc/apache2/sites-available/
 cp -f "$REPO/docker/apache-flask.wsgi" /var/www/apache-flask/app
 
@@ -41,7 +48,4 @@ apache2ctl configtest
 systemctl restart apache2
 systemctl status apache2
 
-
-# Add secret environment variables into /etc/apache2/envvars
-# Or, I could put them in apache-flask.wsgi, using os.environ['FOO'] = 'BAR'.
 # Actually, I think that's the better long-term solution, since then I can ship a config file alongside the git repo. And import it just... using python.
