2859ab1bc816c95c4bdbadacf92a29e7861cccae
Joseph Blackman <joblac@microsoft.com>
2021-09-12 23:55:10 -0700

    More utter nonsense

--- code-deploy.sh
+++ code-deploy.sh
@@ -1,25 +1,25 @@
-# First, clone & download this script
+#!/bin/bash
+# First, clone & run this script
+# mkdir -p /var/www/apache-flask/
+# cd /var/www/apache-flask/
 # git clone https://github.com/jbzdarkid/witness-puzzles
-# chmod u+x ./witness-puzzles/docker/code-deploy.sh
-# sudo ./witness-puzzles/docker/code-deploy.sh
+# chown -R $USER witness-puzzles
+# cd witness-puzzles
+# chmod u+x code-deploy.sh
+# ./code-deploy.sh
+set -e
 
-# Step 1 is actually to *move ourself* into the folder.
-# In fact, I think the long term plan is to move ourselves into /var/ww/apache-flask/witness-puzzes
-REPO="/home/ubuntu/witness-puzzles"
-# Move files into place
-mkdir -p /var/www/apache-flask/
-REPO=$(git rev-parse --show-toplevel)
-mv -rf "$REPO" /var/www/apache-flask/
-chown $USER witness-puzzles
-
+# Install all the dependencies
 apt-get update
 apt-get install -y \
   apache2 \
   build-essential \
+  certbot \
   libapache2-mod-wsgi-py3 \
   msql-client \
   python3-pip \
   python3-venv \
+  requests \
   ruby-full \
   vim \
   wget \
@@ -32,24 +32,21 @@ wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool
 dpkg -i /tmp/chrome.deb
 apt-get autoremove
 
-# if [ ! -d "venv" ]; then
-#   python3 -m venv venv
-# fi
-# 
-# source ./venv/bin/activate
+# Add in python3-certbot-apache
 python3 -m pip install -r "$REPO/requirements.txt"
 
+# Set up and enable flask
 cp -f "$REPO/docker/apache-flask.conf" /etc/apache2/sites-available/
-cp -f "$REPO/docker/apache-flask.wsgi" /var/www/apache-flask/app
-
-# Enable the site
 a2ensite apache-flask
 # a2enmod headers
 a2dissite 000-default.conf
 
-# Test & restart apache
+# Test configuration & restart apache if it passes
 apache2ctl configtest
 systemctl restart apache2
 systemctl status apache2
 
-# Actually, I think that's the better long-term solution, since then I can ship a config file alongside the git repo. And import it just... using python.
+certbot --apache \
+  -n --agree-tos -m "jbzdarkid@gmail.com" \
+  -d witnesspuzzles.com \
+  -d www.witnesspuzzles.com  
