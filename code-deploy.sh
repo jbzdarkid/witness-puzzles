1dad69404c7d6923b84b602457c996c6075f0d78
Joseph Blackman <joblac@microsoft.com>
2021-09-29 04:56:02 -0700

    Various bugs found

--- code-deploy.sh
+++ code-deploy.sh
@@ -1,55 +1,52 @@
-#!/bin/bash
-# First, clone & run this script
-# mkdir -p /var/www/apache-flask/
-# cd /var/www/apache-flask/
-# git clone https://github.com/jbzdarkid/witness-puzzles
-# chown -R $USER witness-puzzles
-# cd witness-puzzles
-# chmod u+x code-deploy.sh
-# ./code-deploy.sh
-set -e
+# curl -sSL https://raw.githubusercontent.com/jbzdarkid/witness-puzzles/master/code-deploy.sh | bash
+
+sudo mkdir -p /var/www/apache-flask/
+sudo chown -R $USER /var/www/apache-flask/
+cd /var/www/apache-flask/
+git clone https://github.com/jbzdarkid/witness-puzzles
+cd witness-puzzles
 
 # Install all the dependencies
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
   apache2 \
-  build-essential \
+  # build-essential \
   certbot \
   libapache2-mod-wsgi-py3 \
-  msql-client \
+  # msql-client \
   python3-pip \
   python3-certbot-apache \
-  requests \
-  ruby-full \
-  vim \
-  wget \
+  # requests \
+  # ruby-full \
+  # wget \
   xdg-utils
 
 # Version list at https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable
-### THIS MUST MATCH THE chromedriver-py MAJOR VERSION! ###
+python3 -m pip install chromedriver-py==92.*
 CHROME_VERSION=92.0.4515.159-1
 wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb
-dpkg -i /tmp/chrome.deb
+sudo dpkg -i /tmp/chrome.deb
+# May need apt --fix-broken install
+rm /tmp/chrome.deb
 apt-get autoremove
 
-python3 -m pip install -r "$REPO/requirements.txt"
+python3 -m pip install -r requirements/prod.txt
 
-# Set up and enable flask
-cp -f "$REPO/docker/witness-puzzles.conf" /etc/apache2/sites-available/
-a2enmod ssl
-a2ensite witness-puzzles
-# a2enmod headers
-a2dissite 000-default.conf
-# a2ensite default-ssl
+# Copy apache configuration, set up apache modules
+sudo cp -f witness-puzzles.conf /etc/apache2/sites-available/
+sudo a2enmod ssl
+sudo a2enmod rewrite
+# mod_wsgi is automatically enabled by the pip install
+sudo a2ensite witness-puzzles
+sudo a2dissite 000-default.conf
 
-# Test configuration & restart apache if it passes
+# Test configuration & restart apache
 apache2ctl configtest
-systemctl restart apache2
-systemctl status apache2
+sudo systemctl restart apache2
+sudo systemctl status apache2
 
-certbot --apache \
+# Enable SSL (autorenew cronjob is created automatically)
+sudo certbot --apache \
   -n --agree-tos -m "jbzdarkid@gmail.com" \
   -d witnesspuzzles.com \
-  -d www.witnesspuzzles.com  
-
-# TODO: Configure certbot auto-renew
+  -d www.witnesspuzzles.com
