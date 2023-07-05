56ce719bd39d669147dca43e7e1cc3b6b47ef66d
jbzdarkid <jbzdarkid@gmail.com>
2023-07-05 16:15:05 -0700

    Moved files

---
+++ .github/workflows/validate_and_publish.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+from time import sleep
+import json
+import os
+import requests
+import subprocess
+
+
+print('Loading puzzle submission...')
+api = f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}'
+
+issue = 9 # os.environ.get('ISSUE_ID')
+headers = {
+    'Accept': 'application/vnd.github.v3+json',
+    'Authorization': 'Bearer ' + os.environ['GITHUB_TOKEN'],
+    'User-Agent': 'Witness Puzzles/3.0 (https://github.com/jbzdarkid/witness-puzzles)',
+}
+j = requests.get(f'{api}/issues/{issue}', headers=headers).json()
+
+if not any([label['name'] == 'new puzzle' for label in j['labels']]):
+    print('This issue was not a puzzle request.')
+    exit(1)
+
+with open('validate.html', 'r', encoding='utf-8') as f:
+    contents = f.read()
+
+tempfile = Path('temp.html').resolve()
+with tempfile.open('w', encoding='utf-8') as f:
+    f.write(contents.replace('%puzzle%', j['body'])) # Let javascript do the object load; we'll be happy with whatever.
+
+print('Installing chrome...')
+chrome_version = '110.0.5481.177-1'
+subprocess.run([
+  'wget',
+  '--no-verbose',
+  '-O', '/tmp/chrome.deb',
+  f'https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_{chrome_version}_amd64.deb',
+], check=True)
+subprocess.run(['sudo', 'dpkg', '-i', '/tmp/chrome.deb'], check=True)
+subprocess.run(['sudo', 'apt', '--fix-broken', 'install', '-y'], check=True)
+
+print('Verifying puzzle...')
+os.environ['LD_LIBRARY_PATH'] = '/opt/google/chrome/lib/:' + os.environ.get('LD_LIBRARY_PATH', '')
+
+# https://developer.chrome.com/articles/new-headless
+dom = subprocess.check_output(['google-chrome-stable', tempfile.as_uri(), '--headless=new', '--dump-dom'], text=True, encoding='utf-8')
+
+print(dom)
+
+for _ in range(60):
+    sleep(1)
+    retcode = p.poll()
+    if retcode:
+        print('Error:', retcode)
+        print('stderr:', p.stderr.read())
+        exit(2)
+        break
+    line = p.stdout.readline()
+    print(line)
+
+
+
+"""
+h = sha256()
+h.update(puzzle_json.encode())
+display_hash = h.hexdigest()[:8].upper()
+display_hash = display_hash.replace('I', 'A')
+display_hash = display_hash.replace('O', 'B')
+display_hash = display_hash.replace('1', 'C')
+display_hash = display_hash.replace('0', 'D')
+puzzle = get_puzzle(display_hash)
+
+with open('play_template.html', 'r', encoding='utf-8') as f:
+    contents = f.read()
+
+contents = contents
+    .replace('%title%', puzzle.name)
+    .replace('%display_hash%', puzzle.display_hash)
+    .replace('%solution%', puzzle.display_hash)
+    .replace('%puzzle%', puzzle.display_hash)
+    .replace('%image%', puzzle.display_hash)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
