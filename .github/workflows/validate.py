352c5e6d38612b3f8a7fa0c09e5f4ca47295beb8
jbzdarkid <jbzdarkid@gmail.com>
2023-07-09 22:00:17 -0700

    maybe this is it?

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -1,21 +1,58 @@
 from pathlib import Path
+import json
 import os
 import subprocess
 
+print('Validating puzzle...')
 contents = open('.github/workflows/template_validate.html', 'r', encoding='utf-8').read()
 puzzle = os.environ['PUZZLE']
-contents = contents.replace('%solution_json%', puzzle) # Let javascript do the object load; we'll be happy with whatever.
+contents = contents.replace('%input_data%', puzzle) # Let javascript do the object load; we'll be happy with whatever.
 
 tempfile = Path('temp.html').resolve()
 with tempfile.open('w', encoding='utf-8') as f:
     f.write(contents)
 
-args = ['google-chrome-stable', tempfile.as_uri(), '--window=size=1200,1200', '--headless=new', '--dump-dom']
+args = ['google-chrome-stable', tempfile.as_uri(), '--headless=new', '--dump-dom']
 dom = subprocess.run(args, text=True, encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True).stdout
 
-start = dom.index('src="data:image/svg+xml;base64,') + len('src="data:image/svg+xml;base64,')
-end   = dom.index('">\n', start)
-img_data = dom[start:end]
-print(img_data)
+data = dom[dom.index('!!!')+3:dom.index('@@@')]
+print(data)
+data = json.loads(data)
+if not data.get('valid', False):
+    print('Puzzle was not valid:', data['error'])
+    exit(1)
+print('Puzzle validated!')
 
-# TODO: Generate files...
+# Extract data from the data (in case we fail, not that we should)
+title = data['title']
+img_bytes = data['screenshot'][len('data:image/png;base64,'):] # Remove prefix
+puzzle_json = data['puzzle_json']
+solution_path = data['solution_path'] # TODO: Encrypt
+
+# This is a slightly updated display_hash solution -- rather than hashing the puzzle, I'm just generating a random ID every time.
+# (Also, I'm flattening the alphabet ahead of time to avoid letter bias.)
+alphabet = ['23456789ABCDEFGHJKLMNPQRSTUVWXYZ']
+display_hash = ''.join(itertools.product(alphabet, 8))
+image_url = f'images/{display_hash[:2]}/{display_hash}.png'
+page_url = f'play/{display_hash}.html'
+
+print('Creating puzzle page...')
+with open(image_url, 'wb') as f:
+    f.write(img_bytes)
+
+contents = open('.github/workflows/template_play.html', 'r', encoding='utf-8').read()
+contents = contents
+    .replace('%title%', title)
+    .replace('%display_hash%', display_hash)
+    .replace('%image_url%', image_url)
+    .replace('%puzzle%', puzzle)
+    .replace('%solution%', solution_path)
+with open(page_url, 'w', encoding='utf-8') as f:
+    f.write(contents)
+
+with open('puzzle_list.js', 'a', encoding='utf-8') as f:
+    contents = f.read().split('\n')
+    contents.insert(1, f'{display_hash}{title}')
+    f.seek(0)
+    f.write('\n'.join(contents))
+
